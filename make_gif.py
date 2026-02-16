548cd53b414a453f5ce671cbe015c166098f07f6
Yamamoto Yuma <calm_stallion3018@gmail.com>
2026-02-16 15:18:51 +0900

    reduce code

--- make_gif.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-import glob
-
-frames = sorted(glob.glob("frames/*.png"))
-
-images = [Image.open(f) for f in frames]
-images[0].save("play.gif", save_all=True, append_images=images[1:], duration=50, loop=0)
